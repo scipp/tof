129594138245535283faf52b628ffe0206ae97b1
Neil Vaytet <neil.vaytet@esss.se>
2023-04-20 18:26:05 +0200

    try import tof for debugging

--- docs/conf.py
+++ docs/conf.py
@@ -8,6 +8,8 @@ src = os.path.abspath('../src')
 os.environ['PYTHONPATH'] = src
 sys.path.insert(0, src)
 
+import tof
+
 # -- Project information -----------------------------------------------------
 
 project = 'tof'
