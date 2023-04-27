5ca6b7ffbd09e5be1b90256859d05d0e5781be68
Neil Vaytet <neil.vaytet@esss.se>
2023-04-27 17:06:43 +0200

    refactor Pulse to use classmethods

--- src/tof/pulse.py
+++ src/tof/pulse.py
@@ -54,113 +54,215 @@ class Pulse:
         lmin: Optional[sc.Variable] = None,
         lmax: Optional[sc.Variable] = None,
         neutrons: int = 1_000_000,
-        kind: Optional[str] = None,
-        p_wav: Optional[np.ndarray] = None,
-        p_time: Optional[np.ndarray] = None,
-        sampling_resolution: int = 10000,
-        birth_times: Optional[sc.Variable] = None,
-        wavelengths: Optional[sc.Variable] = None,
+        generate: bool = True,
     ):
-        self.kind = kind
+        self.birth_times = None
+        self.wavelengths = None
+        self.speeds = None
+        self.kind = None
+
         self.neutrons = neutrons
         self.tmin = tmin
         self.tmax = tmax
         self.lmin = lmin
         self.lmax = lmax
-        self.p_wav = p_wav
-        self.p_time = p_time
-        self.sampling_resolution = sampling_resolution
-        if None not in (birth_times, wavelengths):
-            self.birth_times = birth_times.to(unit='s')
-            self.wavelengths = wavelengths.to(unit='angstrom')
-            self.tmin = self.birth_times.min()
-            self.tmax = self.birth_times.max()
-            self.lmin = self.wavelengths.min()
-            self.lmax = self.wavelengths.max()
-            self.speeds = wavelength_to_speed(self.wavelengths)
-            self.neutrons = len(self.birth_times)
-        elif all(v is None for v in (birth_times, wavelengths)):
-            self.generate()
-        else:
-            raise ValueError(
-                "Either both ``birth_times`` and ``wavelengths`` must be provided, or "
-                "neither."
-            )
 
-    def generate(self, neutrons: Optional[int] = None):
+        if generate:
+            self.birth_times = sc.array(
+                dims=['event'],
+                values=np.random.uniform(
+                    self.tmin.value, self.tmax.value, self.neutrons
+                ),
+                unit='s',
+            )
+            self.wavelengths = sc.array(
+                dims=['event'],
+                values=np.random.uniform(
+                    self.lmin.value, self.lmax.value, self.neutrons
+                ),
+                unit='angstrom',
+            )
+            self.speeds = wavelength_to_speed(self.wavelengths)
+
+    @classmethod
+    def from_neutrons(cls, birth_times: sc.Variable, wavelengths: sc.Variable):
         """
-        Generate neutrons inside the pulse.
+        Create a pulse from a list of neutrons.
+        Both ``birth times`` and ``wavelengths`` should be one-dimensional, and have the
+        same length and dimension label.
 
         Parameters
         ----------
+        birth_times:
+            Birth times of neutrons in the pulse.
+        wavelengths:
+            Wavelengths of neutrons in the pulse.
+        """
+        pulse = cls(generate=False)
+        pulse.birth_times = birth_times.to(unit='s')
+        pulse.wavelengths = wavelengths.to(unit='angstrom')
+        pulse.tmin = pulse.birth_times.min()
+        pulse.tmax = pulse.birth_times.max()
+        pulse.lmin = pulse.wavelengths.min()
+        pulse.lmax = pulse.wavelengths.max()
+        pulse.speeds = wavelength_to_speed(pulse.wavelengths)
+        pulse.neutrons = len(pulse.birth_times)
+        return pulse
+
+    @classmethod
+    def from_facility(
+        cls,
+        kind: str,
+        tmin: Optional[sc.Variable] = None,
+        tmax: Optional[sc.Variable] = None,
+        lmin: Optional[sc.Variable] = None,
+        lmax: Optional[sc.Variable] = None,
+        neutrons: int = 1_000_000,
+        sampling_resolution: int = 10000,
+    ):
+        """
+        Create a pulse from a pre-defined pulse from a neutron facility.
+
+        Parameters
+        ----------
+        kind:
+            Name of a pre-defined pulse from a neutron facility.
+        tmin:
+            Start time of the pulse.
+        tmax:
+            End time of the pulse.
+        lmin:
+            Minimum wavelength of the pulse.
+        lmax:
+            Maximum wavelength of the pulse.
         neutrons:
             Number of neutrons in the pulse.
+        sampling_resolution:
+            Number of points used to sample the probability distributions.
         """
-        if neutrons is not None:
-            self.neutrons = neutrons
-        if self.kind is not None:
-            params = getattr(facilities, self.kind)
-            if self.tmin is None:
-                self.tmin = params.time.coords['time'].min()
-            if self.tmax is None:
-                self.tmax = params.time.coords['time'].max()
-            if self.lmin is None:
-                self.lmin = params.wavelength.coords['wavelength'].min()
-            if self.lmax is None:
-                self.lmax = params.wavelength.coords['wavelength'].max()
-            self.tmin = self.tmin.to(unit='s')
-            self.tmax = self.tmax.to(unit='s')
-            self.lmin = self.lmin.to(unit='angstrom')
-            self.lmax = self.lmax.to(unit='angstrom')
+        pulse = cls(
+            tmin=tmin,
+            tmax=tmax,
+            lmin=lmin,
+            lmax=lmax,
+            neutrons=neutrons,
+            generate=False,
+        )
+        pulse.kind = kind
+        params = getattr(facilities, pulse.kind)
+        if pulse.tmin is None:
+            pulse.tmin = params.time.coords['time'].min()
+        if pulse.tmax is None:
+            pulse.tmax = params.time.coords['time'].max()
+        if pulse.lmin is None:
+            pulse.lmin = params.wavelength.coords['wavelength'].min()
+        if pulse.lmax is None:
+            pulse.lmax = params.wavelength.coords['wavelength'].max()
+        pulse.tmin = pulse.tmin.to(unit='s')
+        pulse.tmax = pulse.tmax.to(unit='s')
+        pulse.lmin = pulse.lmin.to(unit='angstrom')
+        pulse.lmax = pulse.lmax.to(unit='angstrom')
 
+        x_time = np.linspace(pulse.tmin.value, pulse.tmax.value, sampling_resolution)
+        x_wav = np.linspace(pulse.lmin.value, pulse.lmax.value, sampling_resolution)
+        p_time = np.interp(
+            x_time,
+            params.time.coords['time'].to(unit='s').values,
+            params.time.values,
+        )
+        p_time /= p_time.sum()
+        p_wav = np.interp(
+            x_wav,
+            params.wavelength.coords['wavelength'].to(unit='angstrom').values,
+            params.wavelength.values,
+        )
+        p_wav /= p_wav.sum()
+
+        pulse.birth_times = sc.array(
+            dims=['event'],
+            values=np.random.choice(x_time, size=pulse.neutrons, p=p_time),
+            unit='s',
+        )
+        pulse.wavelengths = sc.array(
+            dims=['event'],
+            values=np.random.choice(x_wav, size=pulse.neutrons, p=p_wav),
+            unit='angstrom',
+        )
+        pulse.speeds = wavelength_to_speed(pulse.wavelengths)
+        return pulse
+
+    @classmethod
+    def from_distribution(
+        cls,
+        p_time: sc.DataArray,
+        p_wav: sc.DataArray,
+        neutrons: int = 1_000_000,
+        sampling_resolution: Optional[int] = None,
+    ):
+        """
+        Create a pulse from time a wavelength probability distributions.
+        The distributions should be supplied as DataArrays where the coordinates
+        are the values of the distribution, and the values are the probability.
+
+        Parameters
+        ----------
+        p_time:
+            Time probability distribution.
+        p_wav:
+            Wavelength probability distribution.
+        neutrons:
+            Number of neutrons in the pulse.
+        sampling_resolution:
+            Number of points used to sample the probability distributions. If not set,
+            the size of the distributions will be used.
+        """
+        pulse = cls(
+            tmin=p_time.coords['time'].min().to(unit='s'),
+            tmax=p_time.coords['time'].max().to(unit='s'),
+            lmin=p_wav.coords['wavelength'].min().to(unit='angstrom'),
+            lmax=p_wav.coords['wavelength'].max().to(unit='angstrom'),
+            neutrons=neutrons,
+            generate=False,
+        )
+        p_time = p_time.to(dtype=float)
+        p_wav = p_wav.to(dtype=float)
+
+        if sampling_resolution is None:
+            x_time = p_time.coords['time'].to(dtype=float, unit='s').values
+            p_time = p_time.values
+            x_wav = p_wav.coords['wavelength'].to(dtype=float, unit='angstrom').values
+            p_wav = p_wav.values
+        else:
             x_time = np.linspace(
-                self.tmin.value, self.tmax.value, self.sampling_resolution
+                pulse.tmin.value, pulse.tmax.value, sampling_resolution
             )
-            x_wav = np.linspace(
-                self.lmin.value, self.lmax.value, self.sampling_resolution
-            )
+            x_wav = np.linspace(pulse.lmin.value, pulse.lmax.value, sampling_resolution)
             p_time = np.interp(
                 x_time,
-                params.time.coords['time'].to(unit='s').values,
-                params.time.values,
+                p_time.coords['time'].to(unit='s').values,
+                p_time.values,
             )
-            p_time /= p_time.sum()
             p_wav = np.interp(
                 x_wav,
-                params.wavelength.coords['wavelength'].to(unit='angstrom').values,
-                params.wavelength.values,
-            )
-            p_wav /= p_wav.sum()
-        else:
-            self.tmin = self.tmin.to(unit='s')
-            self.tmax = self.tmax.to(unit='s')
-            self.lmin = self.lmin.to(unit='angstrom')
-            self.lmax = self.lmax.to(unit='angstrom')
-            p_time = self.p_time
-            p_wav = self.p_wav
-
-        if p_time is not None:
-            if self.kind is None:
-                x_time = np.linspace(self.tmin.value, self.tmax.value, len(p_time))
-            self.birth_times = np.random.choice(x_time, size=self.neutrons, p=p_time)
-        else:
-            self.birth_times = np.random.uniform(
-                self.tmin.value, self.tmax.value, self.neutrons
-            )
-        if p_wav is not None:
-            if self.kind is None:
-                x_wav = np.linspace(self.lmin.value, self.lmax.value, len(p_wav))
-            self.wavelengths = np.random.choice(x_wav, size=self.neutrons, p=p_wav)
-        else:
-            self.wavelengths = np.random.uniform(
-                self.lmin.value, self.lmax.value, self.neutrons
+                p_wav.coords['wavelength'].to(unit='angstrom').values,
+                p_wav.values,
             )
 
-        self.birth_times = sc.array(dims=['event'], values=self.birth_times, unit='s')
-        self.wavelengths = sc.array(
-            dims=['event'], values=self.wavelengths, unit='angstrom'
+        p_time = p_time / p_time.sum()
+        p_wav = p_wav / p_wav.sum()
+
+        pulse.birth_times = sc.array(
+            dims=['event'],
+            values=np.random.choice(x_time, size=pulse.neutrons, p=p_time),
+            unit='s',
         )
-        self.speeds = wavelength_to_speed(self.wavelengths)
+        pulse.wavelengths = sc.array(
+            dims=['event'],
+            values=np.random.choice(x_wav, size=pulse.neutrons, p=p_wav),
+            unit='angstrom',
+        )
+        pulse.speeds = wavelength_to_speed(pulse.wavelengths)
+        return pulse
 
     @property
     def duration(self) -> float:
