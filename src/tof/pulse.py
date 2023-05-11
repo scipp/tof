d086d8e4322bd40d1b83b9b68113f2f9a3b7a5cd
Neil Vaytet <neil.vaytet@esss.se>
2023-05-11 08:15:53 +0200

    make facility the default pulse creation mode

--- src/tof/pulse.py
+++ src/tof/pulse.py
@@ -13,6 +13,131 @@ from . import facilities
 from .utils import Plot, wavelength_to_speed
 
 
+def _make_pulse(
+    neutrons: int = 1_000_000,
+    p_time: Optional[sc.DataArray] = None,
+    p_wav: Optional[sc.DataArray] = None,
+    tmin: Optional[sc.Variable] = None,
+    tmax: Optional[sc.Variable] = None,
+    wmin: Optional[sc.Variable] = None,
+    wmax: Optional[sc.Variable] = None,
+    sampling: Optional[int] = 1000,
+):
+    """
+    Create a pulse from time a wavelength probability distributions.
+    The distributions should be supplied as DataArrays where the coordinates
+    are the values of the distribution, and the values are the probability.
+    Note that the time and wavelength distributions are independent. A neutron with
+    a randomly selected birth time from ``p_time`` can adopt any wavelength in
+    ``p_wav`` (in other words, the two distributions are simply broadcast into a
+    square 2D parameter space).
+
+    Parameters
+    ----------
+    p_time:
+        Time probability distribution.
+    p_wav:
+        Wavelength probability distribution.
+    neutrons:
+        Number of neutrons in the pulse.
+    sampling:
+        Number of points used to sample the probability distributions. If not set,
+        the size of the distributions will be used.
+    """
+    if p_time is None:
+        if (tmin is None) and (tmax is None):
+            raise ValueError("Either `p_time` or `tmin` and `tmax` must be specified.")
+        # TODO: set p_time to a uniform distribution between tmin and tmax
+    if p_wav is None:
+        if (wmin is None) and (wmax is None):
+            raise ValueError("Either `p_wav` or `wmin` and `wmax` must be specified.")
+        # TODO: set p_wav to a uniform distribution between wmin and wmax
+
+    t_dim = 'time'
+    w_dim = 'wavelength'
+    t_u = 's'
+    w_u = 'angstrom'
+
+    p_time = p_time.to(dtype=float)
+    p_wav = p_wav.to(dtype=float)
+    p_time.coords[t_dim] = p_time.coords[t_dim].to(unit=t_u)
+    p_wav.coords[w_dim] = p_wav.coords[w_dim].to(unit=w_u)
+
+    if tmin is None:
+        tmin = p_time.coords[t_dim].min()
+    if tmax is None:
+        tmax = p_time.coords[t_dim].max()
+    if wmin is None:
+        wmin = p_wav.coords[w_dim].min()
+    if wmax is None:
+        wmax = p_wav.coords[w_dim].max()
+    tmin = tmin.to(unit=t_u)
+    tmax = tmax.to(unit=t_u)
+    wmin = wmin.to(unit=w_u)
+    wmax = wmax.to(unit=w_u)
+
+    time_interpolator = interp1d(p_time, dim=t_dim, fill_value='extrapolate')
+    wav_interpolator = interp1d(p_wav, dim=w_dim, fill_value='extrapolate')
+    x_time = sc.linspace(
+        dim=t_dim,
+        start=tmin.value,
+        stop=tmax.value,
+        num=sampling,
+        unit=tmin.unit,
+    )
+    x_wav = sc.linspace(
+        dim=w_dim,
+        start=wmin.value,
+        stop=wmax.value,
+        num=sampling,
+        unit=wmin.unit,
+    )
+    p_time = time_interpolator(x_time)
+    p_time /= p_time.data.sum()
+    p_wav = wav_interpolator(x_wav)
+    p_wav /= p_wav.data.sum()
+
+    # In the following, random.choice only allows to select from the values listed
+    # in the coordinate of the probability distribution arrays. This leads to data
+    # grouped into spikes and empty in between because the sampling resolution used
+    # in the linear interpolation above is usually kept low for performance.
+    # To make the distribution more uniform, we add some random noise to the chosen
+    # values, which effectively fills in the gaps between the spikes.
+    # Scipy has some methods to sample from a continuous distribution, but they are
+    # prohibitively slow.
+    # See https://docs.scipy.org/doc/scipy/tutorial/stats/sampling.html for more
+    # information.
+    dt = 0.5 * (tmax - tmin).value / (p_time.sizes[t_dim] - 1)
+    dw = 0.5 * (wmax - wmin).value / (p_wav.sizes[w_dim] - 1)
+
+    birth_times = sc.array(
+        dims=['event'],
+        values=np.random.choice(
+            p_time.coords[t_dim].values, size=neutrons, p=p_time.values
+        )
+        + np.random.uniform(-dt, dt, size=neutrons),
+        unit='s',
+    )
+    wavelengths = sc.array(
+        dims=['event'],
+        values=np.random.choice(
+            p_wav.coords[w_dim].values, size=neutrons, p=p_wav.values
+        )
+        + np.random.uniform(-dw, dw, size=neutrons),
+        unit='angstrom',
+    )
+    speeds = wavelength_to_speed(wavelengths)
+    return {
+        'birth_times': birth_times,
+        'wavelengths': wavelengths,
+        'speeds': speeds,
+        'tmin': tmin,
+        'tmax': tmax,
+        'wmin': wmin,
+        'wmax': wmax,
+    }
+
+
 class Pulse:
     """
     A class that represents a pulse of neutrons.
@@ -20,7 +145,7 @@ class Pulse:
     A probability distribution for the wavelengths and times can be provided (the
     distribution is flat by default).
     In addition, some pre-defined pulses from neutron facilities can be used by setting
-    the ``kind`` parameter.
+    the ``facility`` parameter.
     Finally, the wavelengths and times of the neutrons can be provided directly.
 
     Parameters
@@ -51,7 +176,7 @@ class Pulse:
 
     def __init__(
         self,
-        facility: str = None,
+        facility: str,
         tmin: Optional[sc.Variable] = None,
         tmax: Optional[sc.Variable] = None,
         wmin: Optional[sc.Variable] = None,
@@ -59,39 +184,34 @@ class Pulse:
         neutrons: int = 1_000_000,
         sampling: int = 1000,
     ):
-        self.birth_times = None
-        self.wavelengths = None
-        self.speeds = None
         self.facility = facility
-
         self.neutrons = neutrons
-        self.tmin = tmin
-        self.tmax = tmax
-        self.wmin = wmin
-        self.wmax = wmax
 
-        # if generate:
-        #     self.birth_times = sc.array(
-        #         dims=['event'],
-        #         values=np.random.uniform(
-        #             self.tmin.value, self.tmax.value, self.neutrons
-        #         ),
-        #         unit='s',
-        #     )
-        #     self.wavelengths = sc.array(
-        #         dims=['event'],
-        #         values=np.random.uniform(
-        #             self.wmin.value, self.wmax.value, self.neutrons
-        #         ),
-        #         unit='angstrom',
-        #     )
-        #     self.speeds = wavelength_to_speed(self.wavelengths)
+        if facility is not None:
+            dists = getattr(facilities, facility)
+            params = _make_pulse(
+                tmin=tmin,
+                tmax=tmax,
+                wmin=wmin,
+                wmax=wmax,
+                neutrons=neutrons,
+                p_time=dists.time,
+                p_wav=dists.wavelength,
+                sampling=sampling,
+            )
+            self.birth_times = params['birth_times']
+            self.wavelengths = params['wavelengths']
+            self.speeds = params['speeds']
+            self.tmin = params['tmin']
+            self.tmax = params['tmax']
+            self.wmin = params['wmin']
+            self.wmax = params['wmax']
 
     def __len__(self) -> int:
         """
         Return the number of neutrons in the pulse.
         """
-        return len(self.birth_times)
+        return self.neutrons
 
     @classmethod
     def from_neutrons(cls, birth_times: sc.Variable, wavelengths: sc.Variable):
@@ -107,7 +227,7 @@ class Pulse:
         wavelengths:
             Wavelengths of neutrons in the pulse.
         """
-        pulse = cls(generate=False)
+        pulse = cls(facility=None, neutrons=len(birth_times))
         pulse.birth_times = birth_times.to(unit='s')
         pulse.wavelengths = wavelengths.to(unit='angstrom')
         pulse.tmin = pulse.birth_times.min()
@@ -115,52 +235,6 @@ class Pulse:
         pulse.wmin = pulse.wavelengths.min()
         pulse.wmax = pulse.wavelengths.max()
         pulse.speeds = wavelength_to_speed(pulse.wavelengths)
-        pulse.neutrons = len(pulse.birth_times)
-        return pulse
-
-    @classmethod
-    def from_facility(
-        cls,
-        facility: str,
-        tmin: Optional[sc.Variable] = None,
-        tmax: Optional[sc.Variable] = None,
-        wmin: Optional[sc.Variable] = None,
-        wmax: Optional[sc.Variable] = None,
-        neutrons: int = 1_000_000,
-        sampling: int = 1000,
-    ):
-        """
-        Create a pulse from a pre-defined pulse from a neutron facility.
-
-        Parameters
-        ----------
-        kind:
-            Name of a pre-defined pulse from a neutron facility.
-        tmin:
-            Start time of the pulse.
-        tmax:
-            End time of the pulse.
-        wmin:
-            Minimum wavelength of the pulse.
-        wmax:
-            Maximum wavelength of the pulse.
-        neutrons:
-            Number of neutrons in the pulse.
-        sampling:
-            Number of points used to sample the probability distributions.
-        """
-        params = getattr(facilities, facility)
-        pulse = cls.from_distribution(
-            tmin=tmin,
-            tmax=tmax,
-            wmin=wmin,
-            wmax=wmax,
-            neutrons=neutrons,
-            p_time=params.time,
-            p_wav=params.wavelength,
-            sampling=sampling,
-        )
-        pulse.facility = facility
         return pulse
 
     @classmethod
@@ -196,98 +270,25 @@ class Pulse:
             Number of points used to sample the probability distributions. If not set,
             the size of the distributions will be used.
         """
-        if p_time is None:
-            if (tmin is None) and (tmax is None):
-                raise ValueError(
-                    "Either `p_time` or `tmin` and `tmax` must be specified."
-                )
-        if p_wav is None:
-            if (wmin is None) and (wmax is None):
-                raise ValueError(
-                    "Either `p_wav` or `wmin` and `wmax` must be specified."
-                )
 
-        t_dim = 'time'
-        w_dim = 'wavelength'
-        t_u = 's'
-        w_u = 'angstrom'
-
-        p_time = p_time.to(dtype=float)
-        p_wav = p_wav.to(dtype=float)
-        p_time.coords[t_dim] = p_time.coords[t_dim].to(unit=t_u)
-        p_wav.coords[w_dim] = p_wav.coords[w_dim].to(unit=w_u)
-
-        if tmin is None:
-            tmin = p_time.coords[t_dim].min()
-        if tmax is None:
-            tmax = p_time.coords[t_dim].max()
-        if wmin is None:
-            wmin = p_wav.coords[w_dim].min()
-        if wmax is None:
-            wmax = p_wav.coords[w_dim].max()
-        tmin = tmin.to(unit=t_u)
-        tmax = tmax.to(unit=t_u)
-        wmin = wmin.to(unit=w_u)
-        wmax = wmax.to(unit=w_u)
-        pulse = cls(
+        pulse = cls(facility=None, neutrons=neutrons)
+        params = _make_pulse(
             tmin=tmin,
             tmax=tmax,
             wmin=wmin,
             wmax=wmax,
             neutrons=neutrons,
+            p_time=p_time,
+            p_wav=p_wav,
+            sampling=sampling,
         )
-
-        time_interpolator = interp1d(p_time, dim=t_dim, fill_value='extrapolate')
-        wav_interpolator = interp1d(p_wav, dim=w_dim, fill_value='extrapolate')
-        x_time = sc.linspace(
-            dim=t_dim,
-            start=tmin.value,
-            stop=tmax.value,
-            num=sampling,
-            unit=tmin.unit,
-        )
-        x_wav = sc.linspace(
-            dim=w_dim,
-            start=wmin.value,
-            stop=wmax.value,
-            num=sampling,
-            unit=wmin.unit,
-        )
-        p_time = time_interpolator(x_time)
-        p_time /= p_time.data.sum()
-        p_wav = wav_interpolator(x_wav)
-        p_wav /= p_wav.data.sum()
-
-        # In the following, random.choice only allows to select from the values listed
-        # in the coordinate of the probability distribution arrays. This leads to data
-        # grouped into spikes and empty in between because the sampling resolution used
-        # in the linear interpolation above is usually kept low for performance.
-        # To make the distribution more uniform, we add some random noise to the chosen
-        # values, which effectively fills in the gaps between the spikes.
-        # Scipy has some methods to sample from a continuous distribution, but they are
-        # prohibitively slow.
-        # See https://docs.scipy.org/doc/scipy/tutorial/stats/sampling.html for more
-        # information.
-        dt = 0.5 * (tmax - tmin).value / p_time.sizes[t_dim]
-        dw = 0.5 * (wmax - wmin).value / p_wav.sizes[w_dim]
-
-        pulse.birth_times = sc.array(
-            dims=['event'],
-            values=np.random.choice(
-                p_time.coords[t_dim].values, size=pulse.neutrons, p=p_time.values
-            )
-            + np.random.uniform(-dt, dt, size=pulse.neutrons),
-            unit='s',
-        )
-        pulse.wavelengths = sc.array(
-            dims=['event'],
-            values=np.random.choice(
-                p_wav.coords[w_dim].values, size=pulse.neutrons, p=p_wav.values
-            )
-            + np.random.uniform(-dw, dw, size=pulse.neutrons),
-            unit='angstrom',
-        )
-        pulse.speeds = wavelength_to_speed(pulse.wavelengths)
+        pulse.birth_times = params['birth_times']
+        pulse.wavelengths = params['wavelengths']
+        pulse.speeds = params['speeds']
+        pulse.tmin = params['tmin']
+        pulse.tmax = params['tmax']
+        pulse.wmin = params['wmin']
+        pulse.wmax = params['wmax']
         return pulse
 
     @property
@@ -317,7 +318,7 @@ class Pulse:
             birth_times=self.birth_times,
             wavelengths=self.wavelengths,
             speeds=self.speeds,
-            kind=self.kind,
+            facility=self.facility,
             neutrons=self.neutrons,
             tmin=self.tmin,
             tmax=self.tmax,
@@ -338,7 +339,7 @@ class PulseParameters:
     birth_times: sc.DataArray
     wavelengths: sc.DataArray
     speeds: sc.DataArray
-    kind: Optional[str]
+    facility: Optional[str]
     neutrons: int
     tmin: sc.Variable
     tmax: sc.Variable
