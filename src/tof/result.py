some fixes in plots when some pulses are empty. Also perform one rotation before t=0 for choppers with large phase

src/tof/result.py
@@ -173,11 +173,11 @@ class Result:
         wmin: sc.Variable,
         wmax: sc.Variable,
     ):
-        if max_rays <= 0:
-            return
         da = furthest_detector.data['pulse', pulse_index]
         visible = da[~da.masks['blocked_by_others']]
         tofs = visible.coords['tof']
+        if (max_rays <= 0) or (len(tofs) == 0):
+            return
         if (max_rays is not None) and (len(tofs) > max_rays):
             inds = np.random.choice(len(tofs), size=max_rays, replace=False)
         else:
@@ -203,11 +203,11 @@ class Result:
         furthest_detector: DetectorReading,
         ax: plt.Axes,
     ):
-        if blocked_rays <= 0:
-            return
         slc = ('pulse', pulse_index)
         inv_mask = ~self._masks[furthest_detector.name][slc]
         nrays = int(inv_mask.sum())
+        if (blocked_rays <= 0) or (nrays == 0):
+            return
         if nrays > blocked_rays:
             inds = np.random.choice(nrays, size=blocked_rays, replace=False)
         else:
@@ -313,12 +313,14 @@ class Result:
             self._plot_pulse(pulse_index=i, ax=ax)
 
         det_data = furthest_detector.tofs.visible.data
-        if len(det_data) == 1:
-            tof_max = det_data['pulse:0'].coords['tof'].max().value
+        if sum([da.sum().value for da in det_data.values()]) > 0:
+            tof_max = reduce(
+                max,
+                [da.coords['tof'].max() for da in det_data.values()],
+            ).value
         else:
             tof_max = reduce(
-                lambda a, b: max(a.max(), b.max()),
-                [da.coords['tof'] for da in det_data.values()],
+                max, [ch.close_times.max() for ch in self._choppers.values()]
             ).value
         dx = 0.05 * tof_max
         # Plot choppers
@@ -329,7 +331,11 @@ class Result:
             x[0::3] = x0
             x[1::3] = 0.5 * (x0 + x1)
             x[2::3] = x1
-            x = np.concatenate([[0], x] + ([[tof_max + dx]] if x[-1] < tof_max else []))
+            x = np.concatenate(
+                ([[0]] if x[0] > 0 else [x[0:1]])
+                + [x]
+                + ([[tof_max + dx]] if x[-1] < tof_max else [])
+            )
             y = np.full_like(x, ch.distance.value)
             y[2::3] = None
             ax.plot(x, y, color="k")