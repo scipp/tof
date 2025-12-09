57013f1e418bd6108a52d95664e19872810a2696
Neil Vaytet <neil.vaytet@ess.eu>
2025-12-09 09:33:01 +0100

    refactor tests

--- tests/facilities/ess_test.py
+++ tests/facilities/ess_test.py
@@ -7,18 +7,17 @@ import pytest
 import tof
 
 
-def test_run_dream_model_high_flux():
+@pytest.mark.parametrize("mode", ['high_flux', 'high_resolution'])
+def test_run_dream_model(mode):
     source = tof.Source(facility='ess', neutrons=100_000, pulses=2)
-    dream_params = tof.facilities.ess.dream(high_flux=True)
-    model = tof.Model(source=source, **dream_params)
-    results = model.run()
-    assert len(results.choppers) > 0
-    assert len(results.detectors) > 0
 
+    match mode:
+        case 'high_flux':
+            params = {"high_flux": True}
+        case 'high_resolution':
+            params = {"high_resolution": True}
 
-def test_run_dream_model_high_resolution():
-    source = tof.Source(facility='ess', neutrons=100_000, pulses=2)
-    dream_params = tof.facilities.ess.dream(high_resolution=True)
+    dream_params = tof.facilities.ess.dream(**params)
     model = tof.Model(source=source, **dream_params)
     results = model.run()
     assert len(results.choppers) > 0
@@ -26,18 +25,9 @@ def test_run_dream_model_high_resolution():
 
 
 @pytest.mark.parametrize("pulse_skipping", [True, False])
-def test_run_odin_model(pulse_skipping):
-    source = tof.Source(facility='ess', neutrons=100_000, pulses=2)
-    odin_params = tof.facilities.ess.odin(pulse_skipping=pulse_skipping)
-    model = tof.Model(source=source, **odin_params)
-    results = model.run()
-    assert len(results.choppers) > 0
-    assert len(results.detectors) > 0
-
-
-@pytest.mark.parametrize("pulse_skipping", [True, False])
-def test_run_odin_model_with_odin_source(pulse_skipping):
-    source = tof.Source(facility='ess-odin', neutrons=100_000, pulses=2)
+@pytest.mark.parametrize("facility", ['ess', 'ess-odin'])
+def test_run_odin_model(pulse_skipping, facility):
+    source = tof.Source(facility=facility, neutrons=100_000, pulses=2)
     odin_params = tof.facilities.ess.odin(pulse_skipping=pulse_skipping)
     model = tof.Model(source=source, **odin_params)
     results = model.run()
