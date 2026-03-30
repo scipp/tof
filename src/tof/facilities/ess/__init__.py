ce96ae9241b5eed5fc811888c74c779ee2811894
Neil Vaytet <neil.vaytet@ess.eu>
2026-03-31 00:03:22 +0200

    fix md5 hashes and test

--- src/tof/facilities/ess/__init__.py
+++ src/tof/facilities/ess/__init__.py
@@ -27,70 +27,70 @@ from .magic import magic
 from .odin import odin
 
 sources = {
-    "ess": {"path": "ess/ess.h5", "hash": "md5:689630334ae9a7f462f76943740a8ff2"},
+    "ess": {"path": "ess/ess.h5", "hash": "md5:4d929bf73ba808f8a63ebaf137e4b2fc"},
+    "ess-beer": {
+        "path": "ess/ess-beer.h5",
+        "hash": "md5:9e4350a13c6f322ebe788ce874787a3b",
+    },
+    "ess-bifrost": {
+        "path": "ess/ess-bifrost.h5",
+        "hash": "md5:83952831dc576124b8a4de6593c09c64",
+    },
+    "ess-cspec": {
+        "path": "ess/ess-cspec.h5",
+        "hash": "md5:09e7ed297aefcddc4eb5a63146629b22",
+    },
+    "ess-dream": {
+        "path": "ess/ess-dream.h5",
+        "hash": "md5:ddd18500e4bd627fb6fca7d010bec019",
+    },
+    "ess-estia": {
+        "path": "ess/ess-estia.h5",
+        "hash": "md5:2c6a0af4559be482bb30c67131d25be4",
+    },
+    "ess-freia": {
+        "path": "ess/ess-freia.h5",
+        "hash": "md5:58fd7a0c720f4ce17a1d5713b02d9f96",
+    },
+    "ess-heimdal": {
+        "path": "ess/ess-heimdal.h5",
+        "hash": "md5:40925e3267acaa8b6efcd05b67881107",
+    },
+    "ess-loki": {
+        "path": "ess/ess-loki.h5",
+        "hash": "md5:6727cd93ea8775cd2ac6d8ce3c3446e8",
+    },
+    "ess-magic": {
+        "path": "ess/ess-magic.h5",
+        "hash": "md5:de2989fd6123f3d4cda6a1c2c13a0e49",
+    },
+    "ess-miracles": {
+        "path": "ess/ess-miracles.h5",
+        "hash": "md5:5d33490ae632a1ee33d617f0daf050a7",
+    },
+    "ess-nmx": {
+        "path": "ess/ess-nmx.h5",
+        "hash": "md5:dca26e529f01b8e8f55b5611802d36a5",
+    },
     "ess-odin": {
         "path": "ess/ess-odin.h5",
         "hash": "md5:4d64102d2a743bd7672967ad0cb94872",
     },
-    "ess-nmx": {
-        "path": "ess/ess-nmx.h5",
-        "hash": "md5:c8a3d0c6ef17c704c94afc38b031fa60",
-    },
-    "ess-beer": {
-        "path": "ess/ess-beer.h5",
-        "hash": "md5:7d98d4446f53f4c47c3d632ae2f886b7",
-    },
-    "ess-cspec": {
-        "path": "ess/ess-cspec.h5",
-        "hash": "md5:8f8268c3b8ae6219c595b9d099ee632d",
-    },
-    "ess-bifrost": {
-        "path": "ess/ess-bifrost.h5",
-        "hash": "md5:c05b50d2a3a919e10fb0abd44144c83d",
-    },
-    "ess-miracles": {
-        "path": "ess/ess-miracles.h5",
-        "hash": "md5:3760666ce2685ff88bb79cec8d19ef8c",
-    },
-    "ess-magic": {
-        "path": "ess/ess-magic.h5",
-        "hash": "md5:774e99c8130075139b8476a87fb43182",
-    },
-    "ess-trex": {
-        "path": "ess/ess-trex.h5",
-        "hash": "md5:1eb645ec2e71324c2d03a30c4f3525fb",
-    },
-    "ess-heimdal": {
-        "path": "ess/ess-heimdal.h5",
-        "hash": "md5:e95da29df2f478eb7357583b8c388123",
-    },
-    "ess-dream": {
-        "path": "ess/ess-dream.h5",
-        "hash": "md5:0957767f7439f70aaf1abf934d30da07",
-    },
-    "ess-loki": {
-        "path": "ess/ess-loki.h5",
-        "hash": "md5:319936b29e3e8ac70283408950e6195c",
-    },
-    "ess-freia": {
-        "path": "ess/ess-freia.h5",
-        "hash": "md5:cad1a612214d0155b060094b1078f254",
-    },
-    "ess-estia": {
-        "path": "ess/ess-estia.h5",
-        "hash": "md5:a0bdf9e17611a77054686c5041aaae3f",
-    },
     "ess-skadi": {
         "path": "ess/ess-skadi.h5",
-        "hash": "md5:521dc300296db137fa3dd5877882f68c",
-    },
-    "ess-vespa": {
-        "path": "ess/ess-vespa.h5",
-        "hash": "md5:298f599f7dc861d1319748c7ea3c54fc",
+        "hash": "md5:eab4c20f95ed2a63479407a54a01cca9",
     },
     "ess-tbl": {
         "path": "ess/ess-tbl.h5",
-        "hash": "md5:c9db7f205131c4caf491d5951253bea7",
+        "hash": "md5:8af1a26d386f690e89ef23f35daeef13",
+    },
+    "ess-trex": {
+        "path": "ess/ess-trex.h5",
+        "hash": "md5:3f6f77ce4b301867acb334b7378b7fe1",
+    },
+    "ess-vespa": {
+        "path": "ess/ess-vespa.h5",
+        "hash": "md5:eac9c4c4b861090426f0ed3c2e2b479d",
     },
 }
 
